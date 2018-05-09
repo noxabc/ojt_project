1d544f3e7bf4c9567f363f8bb46f2eac21f960e3
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-05-09 10:49:31 +0800

    REMOVED t.references :profile, foreign_key: true

--- db/migrate/20180502111124_create_balance_loads.rb
+++ db/migrate/20180502111124_create_balance_loads.rb
@@ -3,7 +3,7 @@ class CreateBalanceLoads < ActiveRecord::Migration[5.2]
     create_table :balance_loads do |t|
       t.integer :amount_loaded
       t.integer :remaining_load
-      t.references :profile, foreign_key: true
+
 
       t.timestamps
     end
