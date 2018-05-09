7478b6ec4731c0003004f690e0f473092da709f5
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-05-09 10:48:59 +0800

    REMOVED t.references :profile, foreign_key: true

--- db/migrate/20180502111007_create_trip_transactions.rb
+++ db/migrate/20180502111007_create_trip_transactions.rb
@@ -4,7 +4,7 @@ class CreateTripTransactions < ActiveRecord::Migration[5.2]
       t.string :pickup_location
       t.string :drop_location
       t.integer :trip_amount
-      t.references :profile, foreign_key: true
+
 
       t.timestamps
     end
