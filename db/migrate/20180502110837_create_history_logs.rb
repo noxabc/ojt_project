98f8ec365e21c487e322fb3f1c0438dbef2cbffe
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-05-09 10:48:07 +0800

    REMOVED t.references :profile, foreign_key: true

--- db/migrate/20180502110837_create_history_logs.rb
+++ db/migrate/20180502110837_create_history_logs.rb
@@ -3,7 +3,7 @@ class CreateHistoryLogs < ActiveRecord::Migration[5.2]
     create_table :history_logs do |t|
       t.string :last_name
       t.string :first_name
-      t.references :profile, foreign_key: true
+      
 
       t.timestamps
     end
