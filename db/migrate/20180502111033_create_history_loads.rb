9cf4698df6eeec04dbbfd77c50329e6d314ebd7a
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-05-09 10:49:13 +0800

    REMOVED t.references :profile, foreign_key: true

--- db/migrate/20180502111033_create_history_loads.rb
+++ db/migrate/20180502111033_create_history_loads.rb
@@ -2,7 +2,7 @@ class CreateHistoryLoads < ActiveRecord::Migration[5.2]
   def change
     create_table :history_loads do |t|
       t.integer :amount_loaded
-      t.references :profile, foreign_key: true
+
 
       t.timestamps
     end
