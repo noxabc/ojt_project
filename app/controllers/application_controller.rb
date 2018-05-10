e6dad441fb36b007f104c1e7411791c17c7e8d63
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-05-10 14:18:58 +0800

    Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  # protect_from_forgery with: :null_session
+  # attr_reader :current_user
+  # private
+  #
+  # def authenticate_user!
+  #   _, token, email = request.headers['Authorization'].split('')
+  #   user = User.where("email = ? and tokens like ?", email, "%#{token}%").first
+  #   if user.present? && user.has_valid_token?(token)
+  #     @current_user = user
+  #   else
+  #     render json: { error: 'Invalid Authorization token'}, status: :unauthorized
+  #   end
+  # end
 end
