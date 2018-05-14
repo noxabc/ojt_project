44c5acc9523f627ec176ef23e949bb4f5f7e4092
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-05-14 16:55:26 +0800

    Added user.regenerate_auth_token

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,14 +1,13 @@
 class SessionController < ApplicationController
 
   def create
-    user = User.find_by(email: params[:email])
-
+    user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      # token = user.generate_token
-      # render json: token, status: :ok
+      user.regenerate_auth_token
       render json: { status: 'Success', data:user}, status: :ok
     else
       render json: { error: "Wrong email or password" }, status: :unauthorized
     end
   end
+
 end
