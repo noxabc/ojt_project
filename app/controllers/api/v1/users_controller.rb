86fdf56ae817af588cc446ab7ba77c8e0e47c81a
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-05-15 11:49:17 +0800

    User Controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,36 +4,21 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     users = User.order('created_at DESC');
-    render json: { status: 'SUCCESS!', message:'Loaded Users', data:users },status: :ok
+    render json: { status: 'Success', message:'Loaded Users', data:users },status: :ok
   end
 
   def create
     user = User.new(users_params)
     if user.save
-      # render json:{} user, status: :ok
-      render json: { status: 'SUCCESS!', message:'Loaded Users', data:user },status: :ok
+      render json: { status: 'Success', message:'Loaded Users', data:user },status: :ok
     else
       render json: user.errors, status: :bad_request
     end
   end
-
-  # def update
-  #   if current_user.update(users_params)
-  #     render json: current_user.reload, status: :ok
-  #   else
-  #     render json: current_user.errors, status: 406
-  #   end
-  # end
 
   private
 
   def users_params
     params.permit(:last_name, :first_name, :middle_name, :email, :password, :password_confirmation, :mobile_number)
   end
-
-  # def authorize_self
-  #   unless current_user.id.to_s == params[:id]
-  #     render json: { error: 'You are not authorized to perform the action'}, status: :forbidden
-  #   end
-  # end
 end
