942754fc24868d8c39d2d002d1e728e05c926b4f
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-05-15 11:52:00 +0800

    User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
   has_secure_token :auth_token
-  # validates_uniqueness_of :email
 
+  # validates :last_name
+  # validates :first_name
+  # validates :middle_name
+  # validates :email
+  # validates_uniqueness_of :email
+  # validates :password
+  # validates :mobile_number
 end
