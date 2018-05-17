42f152c3d48a33bfada9eaaabe4dabbb44725799
Jan Karlo Gacutara <jankarlogacs@gmail.com>
2018-05-17 12:46:24 +0800

    Added Update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,23 @@
 class Api::V1::UsersController < ApplicationController
-  # before_action :authenticate_user!, only: :update
-  # before_action :authorize_self, only: :update
 
   def index
     users = User.order('created_at DESC');
-    render json: { status: 'Success', message:'Loaded Users', data:users },status: :ok
+    render json: users.reload, status: :ok
   end
 
   def create
     user = User.new(users_params)
     if user.save
-      render json: { status: 'Success', message:'Created User/s', data:user },status: :ok
+      render json: user.reload, status: :ok
+    else
+      render json: user.errors, status: :bad_request
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(users_params)
+      render json: user.reload, status: :ok
     else
       render json: user.errors, status: :bad_request
     end
